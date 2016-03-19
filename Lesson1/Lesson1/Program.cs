75a9622df0a971e142dfa826cd41b26329480727
Yuliya Sedletskaya <Yuliya Sedletskaya>
2016-03-19 15:33:05 +0200

    Lesson1 full solution

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        public static int InputNumber(string a)
+        {
+            Console.WriteLine("Please enter {0}:", a);
+            //Reading input number
+            
+            string input = Console.ReadLine();
+
+            int inputAsNumber;
+            
+            if (int.TryParse(input, out inputAsNumber))
+            {
+                return inputAsNumber;
+            }
+            Console.WriteLine("You entered an invalid number");
+            return 0;
+        }
+
+        private static void Main(string[] args)
+        {
+            int numberA = InputNumber("A");
+            int numberB = InputNumber("B");
+            if (numberA%numberB == 0)
+            {
+                Console.WriteLine("B is divider of A");
+            }
+            else
+            {
+                Console.WriteLine("B is NOT diveder of A");
+            }
+            Console.ReadKey();
+        }
+    }
+}
